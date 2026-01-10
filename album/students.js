ddee4b10a2f4318cfaa653512802a77f3921ee1f
lukyyy97 <redmidima37@gmail.com>
2026-01-11 00:03:26 +0200

    Update students.js

--- album/students.js
+++ album/students.js
@@ -1,1 +1,38 @@
+// 1) Фото кидай у /assets/album/
+// 2) Тут просто заповнюй: photo, lastName, firstName, birthday (ДД.ММ), quote
+// 3) Перша людина має leader: true (класний керівник)
 
+window.STUDENTS = [
+  {
+    leader: true,
+    role: "Класний керівник",
+    photo: "assets/album/teacher.jpg",
+    lastName: "Прізвище",
+    firstName: "Ім’я",
+    birthday: "01.09",
+    quote: "test text"
+  },
+
+  { photo: "assets/album/1.jpg",  lastName: "Прізвище", firstName: "Ім’я", birthday: "02.01", quote: "test text" },
+  { photo: "assets/album/2.jpg",  lastName: "Прізвище", firstName: "Ім’я", birthday: "03.01", quote: "test text" },
+  { photo: "assets/album/3.jpg",  lastName: "Прізвище", firstName: "Ім’я", birthday: "04.01", quote: "test text" },
+  { photo: "assets/album/4.jpg",  lastName: "Прізвище", firstName: "Ім’я", birthday: "05.01", quote: "test text" },
+  { photo: "assets/album/5.jpg",  lastName: "Прізвище", firstName: "Ім’я", birthday: "06.01", quote: "test text" },
+  { photo: "assets/album/6.jpg",  lastName: "Прізвище", firstName: "Ім’я", birthday: "07.01", quote: "test text" },
+  { photo: "assets/album/7.jpg",  lastName: "Прізвище", firstName: "Ім’я", birthday: "08.01", quote: "test text" },
+  { photo: "assets/album/8.jpg",  lastName: "Прізвище", firstName: "Ім’я", birthday: "09.01", quote: "test text" },
+  { photo: "assets/album/9.jpg",  lastName: "Прізвище", firstName: "Ім’я", birthday: "10.01", quote: "test text" },
+  { photo: "assets/album/10.jpg", lastName: "Прізвище", firstName: "Ім’я", birthday: "11.01", quote: "test text" },
+  { photo: "assets/album/11.jpg", lastName: "Прізвище", firstName: "Ім’я", birthday: "12.01", quote: "test text" },
+  { photo: "assets/album/12.jpg", lastName: "Прізвище", firstName: "Ім’я", birthday: "13.01", quote: "test text" },
+  { photo: "assets/album/13.jpg", lastName: "Прізвище", firstName: "Ім’я", birthday: "14.01", quote: "test text" },
+  { photo: "assets/album/14.jpg", lastName: "Прізвище", firstName: "Ім’я", birthday: "15.01", quote: "test text" },
+  { photo: "assets/album/15.jpg", lastName: "Прізвище", firstName: "Ім’я", birthday: "16.01", quote: "test text" },
+  { photo: "assets/album/16.jpg", lastName: "Прізвище", firstName: "Ім’я", birthday: "17.01", quote: "test text" },
+  { photo: "assets/album/17.jpg", lastName: "Прізвище", firstName: "Ім’я", birthday: "18.01", quote: "test text" },
+  { photo: "assets/album/18.jpg", lastName: "Прізвище", firstName: "Ім’я", birthday: "19.01", quote: "test text" },
+  { photo: "assets/album/19.jpg", lastName: "Прізвище", firstName: "Ім’я", birthday: "20.01", quote: "test text" },
+  { photo: "assets/album/20.jpg", lastName: "Прізвище", firstName: "Ім’я", birthday: "21.01", quote: "test text" },
+  { photo: "assets/album/21.jpg", lastName: "Прізвище", firstName: "Ім’я", birthday: "22.01", quote: "test text" },
+  { photo: "assets/album/22.jpg", lastName: "Прізвище", firstName: "Ім’я", birthday: "23.01", quote: "test text" }
+];
