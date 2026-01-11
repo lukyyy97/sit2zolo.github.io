087b79d6bb43c942f31aa2319a681b0fda203077
lukyyy97 <redmidima37@gmail.com>
2026-01-11 12:53:26 +0200

    Update students.js

--- album/students.js
+++ album/students.js
@@ -9,7 +9,7 @@ window.STUDENTS = [
     photo: "assets/album/teacher.jpg",
     lastName: "Лискова",
     firstName: "Світлана",
-    birthday: "01.09",
+    birthday: "22.04",
     quote: "test text"
   },
 { photo: "assets/album/1.jpg",  lastName: "Андрієнко", firstName: "Крістіна",   birthday: "02.01", quote: "test text" },
@@ -27,7 +27,7 @@ window.STUDENTS = [
 { photo: "assets/album/13.jpg", lastName: "Куліш",     firstName: "Анна",       birthday: "14.01", quote: "test text" },
 { photo: "assets/album/14.jpg", lastName: "Медвідь",   firstName: "Паша",       birthday: "15.01", quote: "test text" },
 { photo: "assets/album/15.jpg", lastName: "Мозгова",   firstName: "Марина",     birthday: "16.01", quote: "Не ідеальна, зате справжня" },
-{ photo: "assets/album/16.jpg", lastName: "Немирський",firstName: "Владислав",  birthday: "17.01", quote: "test text" },
+{ photo: "assets/album/16.jpg", lastName: "Немирський",firstName: "Владислав",  birthday: "17.01", quote: "Переможець - це той самий програвший, який просто спробував ще раз" },
 { photo: "assets/album/17.jpg", lastName: "Падалка",   firstName: "Роман",      birthday: "18.01", quote: "test text" },
 { photo: "assets/album/18.jpg", lastName: "Пашкевич",  firstName: "Анастасія",  birthday: "19.01", quote: "Я йду  далі не тому, що все знаю, а тому що вірю в себе " },
 { photo: "assets/album/19.jpg", lastName: "Савицький", firstName: "Денис",      birthday: "20.01", quote: "Я живу так, як відчуваю, а не як від мене чекають." },
