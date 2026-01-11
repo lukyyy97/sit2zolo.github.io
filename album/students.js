22db6e77df81fe816d78a5592b740c9d56f28b54
lukyyy97 <redmidima37@gmail.com>
2026-01-11 20:44:13 +0200

    Update students.js

--- album/students.js
+++ album/students.js
@@ -6,7 +6,7 @@ window.STUDENTS = [
   {
     leader: true,
     role: "Класний керівник",
-    photo: "assets/album/teacher.jpg",
+    photo: "assets/album/1.jpg",
     lastName: "Лискова",
     firstName: "Світлана",
     birthday: "22.04",
